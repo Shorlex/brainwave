977af13b7050a12d4b3ec90baccb3f4d44110d05
Abdulkadir Abubakar <abdulkaadiras@gmail.com>
2024-05-02 10:35:06 +0100

    Update SVG

--- src/constants/index.js
+++ src/constants/index.js
@@ -260,7 +260,7 @@ export const benefits = [
     id: "0",
     title: "Portfolio Management",
     text: "With Flutter Funds, you gain access to professional portfolio management typically reserved for high-net-worth individuals.",
-    backgroundUrl: "./src/assets/benefits/card-1.png",
+    backgroundUrl: benefitCard1,
     iconUrl: benefitIcon1,
     imageUrl: benefitImage2,
   },
